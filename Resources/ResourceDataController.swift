a38376e90827ea8a70138f222083afe0e17af08a
Cara Weggler <cweggler@gmail.com>
2019-03-19 18:20:58 -0500

    set up the ResourceDataController class

--- Resources/ResourceDataController.swift
+++ Resources/ResourceDataController.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class ResourceDataController {
+    
+    let resources: [Resource]
+    
+    init() {
+        let pListURL = Bundle.main.path(forResource: "Resources", ofType: ".plist")
+        let resourcesPlist = NSArray(contentsOfFile: pListURL!)
+        
+        var resources: [Resource] = []
+        
+        for r in resourcesPlist! {
+            let dict = r as! [String: String]
+            let res = Resource(dictionary: dict)
+            resources.append(res)
+        }
+        
+        self.resources = resources
+    }
+}

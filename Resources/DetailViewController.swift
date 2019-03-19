43fb4e7f671aa1d3b25876648f87c8eb2fa27478
Cara Weggler <cweggler@gmail.com>
2019-03-19 18:55:05 -0500

    wire up Label outlets to match the pList

--- Resources/DetailViewController.swift
+++ Resources/DetailViewController.swift
@@ -11,4 +11,17 @@ import UIKit
 class DetailViewController: UIViewController {
     
     var resource: Resource!
+    
+    @IBOutlet var name: UILabel!
+    @IBOutlet var descr: UILabel!
+    @IBOutlet var url: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        name!.text = resource.name
+        descr.text = resource.description
+        url.text = resource.url.absoluteString
+    }
+    
 }

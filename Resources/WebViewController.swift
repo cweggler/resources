a07689e2c098e6ea95acfea3557ec56c44a5f872
Cara Weggler <cweggler@gmail.com>
2019-03-19 19:03:23 -0500

    wired up WKWebView

--- Resources/WebViewController.swift
+++ Resources/WebViewController.swift
@@ -6,8 +6,12 @@
 //  Copyright © 2019 Cara. All rights reserved.
 //
 
+import WebKit
 import UIKit
 
 class WebViewController: UIViewController {
     
+    var url: URL!
+    
+    @IBOutlet var webView: WKWebView!
 }
